4eaa9f439cc9c9dddc7187c456b5e0eaf245d031
KevinMassey <kevin.massey@ontariotechu.net>
2025-11-23 22:56:18 -0500

    Adding Heroku Deployment, TEST

--- app.js
+++ app.js
@@ -21,4 +21,5 @@ mongoose.connect(
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
